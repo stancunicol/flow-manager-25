9c1a5a98f5d45d371303358885ce088b321c9dc8
AdelinaCostea <adelinacostea41@gmail.com>
2025-07-16 08:45:29 +0300

    Update Step Entity to include update history

--- FlowManager/Domain/Entities/Step.cs
+++ FlowManager/Domain/Entities/Step.cs
@@ -5,7 +5,9 @@
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
         public List<Guid> AssignedUserIds { get; set; } = new List<Guid>();
+        public Dictionary<Guid, DateTime> UpdateHistory { get; set; } = new Dictionary<Guid, DateTime>();
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        
     }
 }
