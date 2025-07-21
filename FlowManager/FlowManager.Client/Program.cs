93ce234d5cb969d508ffa71cd6493bec704127c0
Mihai <mihai.marinescu2112@yahoo.com>
2025-07-21 10:26:53 +0300

    Added services

--- FlowManager/FlowManager.Client/Program.cs
+++ FlowManager/FlowManager.Client/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FlowManager.Client;
+using FlowManager.Infrastructure.Services;
+using FlowManager.Application.Interfaces;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Added services for FormService and FlowService
+builder.Services.AddScoped<IFlowService, FlowService>();
+builder.Services.AddScoped<IFormService, FormService>();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 
 await builder.Build().RunAsync();
