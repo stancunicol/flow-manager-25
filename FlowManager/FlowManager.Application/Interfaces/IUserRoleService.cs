1c2fae392c703daf051c9db2c408a5db7b0c674b
AdelinaCostea <adelinacostea41@gmail.com>
2025-07-21 10:22:43 +0300

    Added IUserRoleService and UserRoleService

---
+++ FlowManager/FlowManager.Application/Interfaces/IUserRoleService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowManager.Domain.Entities;
+
+namespace FlowManager.Application.Interfaces
+{
+    public interface IUserRoleService
+    {
+        Task<IEnumerable<UserRole>> GetAllUserRoles();
+        Task<UserRole?> GetUserRole(Guid id);
+        Task<IEnumerable<UserRole>> GetRolesByUser(Guid userId);
+        Task<IEnumerable<UserRole>> GetUsersByRole(RoleType roleType);
+        Task<bool> UpdateUserRole(Guid id, UserRole userRole);
+        Task<UserRole?> CreateUserRole(UserRole userRole);
+        Task<bool> DeleteUserRole(Guid id);
+    }
+}
---
+++ FlowManager/FlowManager.Infrastructure/Services/UserRoleService.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowManager.Application.Interfaces;
+using FlowManager.Domain.Entities;
+using FlowManager.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowManager.Infrastructure.Services
+{
+    public class UserRoleService
+    {
+       
+            private readonly AppDbContext _context;
+
+            public UserRoleService(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IEnumerable<UserRole>> GetAllUserRoles()
+            {
+                return await _context.UserRoles
+                    .Include(ur => ur.User)
+                    .ToListAsync();
+            }
+
+            public async Task<UserRole?> GetUserRole(Guid id)
+            {
+                return await _context.UserRoles
+                    .Include(ur => ur.User)
+                    .FirstOrDefaultAsync(ur => ur.Id == id);
+            }
+
+            public async Task<IEnumerable<UserRole>> GetRolesByUser(Guid userId)
+            {
+                return await _context.UserRoles
+                    .Include(ur => ur.User)
+                    .Where(ur => ur.UserId == userId)
+                    .ToListAsync();
+            }
+
+            public async Task<IEnumerable<UserRole>> GetUsersByRole(RoleType roleType)
+            {
+                return await _context.UserRoles
+                    .Include(ur => ur.User)
+                    .Where(ur => ur.Role == roleType)
+                    .ToListAsync();
+            }
+
+            public async Task<bool> UpdateUserRole(Guid id, UserRole userRole)
+            {
+                if (id != userRole.Id) return false;
+
+                _context.Entry(userRole).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return true;
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return await _context.UserRoles.AnyAsync(e => e.Id == id);
+                }
+            }
+
+            public async Task<UserRole?> CreateUserRole(UserRole userRole)
+            {
+                if (await _context.UserRoles.AnyAsync(ur => ur.UserId == userRole.UserId && ur.Role == userRole.Role))
+                {
+                    return null;
+                }
+
+                userRole.Id = Guid.NewGuid();
+                userRole.AssignedAt = DateTime.UtcNow;
+
+                _context.UserRoles.Add(userRole);
+                await _context.SaveChangesAsync();
+
+                return userRole;
+            }
+
+            public async Task<bool> DeleteUserRole(Guid id)
+            {
+                var userRole = await _context.UserRoles.FindAsync(id);
+                if (userRole == null) return false;
+
+                _context.UserRoles.Remove(userRole);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+        }
+ }
