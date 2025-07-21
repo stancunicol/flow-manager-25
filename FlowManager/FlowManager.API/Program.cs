4cc7ffd62c43ca9f0f838e16f539df0d65f55774
Mihai <mihai.marinescu2112@yahoo.com>
2025-07-21 13:11:21 +0300

    Added Authorization and Authentication + Cookie + Swagger

--- FlowManager/FlowManager.API/Program.cs
+++ FlowManager/FlowManager.API/Program.cs
@@ -1,5 +1,8 @@
 using FlowManager.Application;
+using FlowManager.Domain.Entities;
 using FlowManager.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +12,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication();
+
+builder.Services.ConfigureApplicationCookie(Options => 
+{
+    Options.LoginPath = "/api/auth/login";
+    Options.LogoutPath = "/api/auth/logout";
+    Options.AccessDeniedPath = "/api/auth/denied";
+    Options.ExpireTimeSpan = TimeSpan.FromDays(30);
+});
+
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
 
 var app = builder.Build();
 
@@ -19,9 +40,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.MapIdentityApi<User>();
 
 app.UseAuthentication();
 app.UseAuthorization();
--- FlowManager/FlowManager.Client/Program.cs
+++ FlowManager/FlowManager.Client/Program.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FlowManager.Client;
+using Microsoft.AspNetCore.Identity;
 using FlowManager.Infrastructure.Services;
 using FlowManager.Application.Interfaces;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+var app = builder.Build();
+
 
 // Added services for FormService and FlowService
 builder.Services.AddScoped<IFlowService, FlowService>();
