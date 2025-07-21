6e1bc880642d666b90c5fc0352366d003b197d8d
Mihai <mihai.marinescu2112@yahoo.com>
2025-07-21 10:26:27 +0300

    Added IFlowService and IFormService

---
+++ FlowManager/FlowManager.Application/Interfaces/IFormService.cs
@@ -0,0 +1,15 @@
+using FlowManager.Domain.Entities;
+
+namespace FlowManager.Application.Interfaces
+{
+    public interface IFormService
+    {
+        Task<IEnumerable<Form>> GetAllFormsAsync();
+        Task<Form?> GetFormByIdAsync(Guid id);
+        Task<IEnumerable<Form>> GetFormsByFlowAsync(Guid flowId);
+        Task<IEnumerable<Form>> GetFormsByUserAsync(Guid userId);
+        Task<Form> CreateFormAsync(Form form);
+        Task<bool> UpdateFormAsync(Guid id, Form form);
+        Task<bool> DeleteFormAsync(Guid id);
+    }
+}
