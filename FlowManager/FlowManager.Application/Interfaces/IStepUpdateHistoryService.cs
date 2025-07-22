d1889d5b503e891e857ae1bf21ad3d33d8218a6b
AdelinaCostea <adelinacostea41@gmail.com>
2025-07-22 10:12:24 +0300

    Added StepUpdateHistoriesController, StepUpdateHistory and IStepUpdateHistory

---
+++ FlowManager/FlowManager.Application/Interfaces/IStepUpdateHistoryService.cs
@@ -0,0 +1,20 @@
+﻿using FlowManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowManager.Application.Interfaces
+{
+    public interface IStepUpdateHistoryService
+    {
+        Task<IEnumerable<StepUpdateHistory>> GetStepUpdateHistories();
+        Task<StepUpdateHistory?> GetStepUpdateHistory(Guid id);
+        Task<IEnumerable<StepUpdateHistory>> GetHistoriesByStep(Guid stepId);
+        Task<IEnumerable<StepUpdateHistory>> GetHistoriesByUser(Guid userId);
+        Task<StepUpdateHistory> PostStepUpdateHistory(StepUpdateHistory stepUpdateHistory);
+        Task<bool> PutStepUpdateHistory(Guid id, StepUpdateHistory stepUpdateHistory);
+        Task<bool> DeleteStepUpdateHistory(Guid id);
+    }
+}
