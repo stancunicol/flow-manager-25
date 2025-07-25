89e17b1539e3e1024db5edf4e6e386fe74c35a92
Mihai <mihai.marinescu2112@yahoo.com>
2025-07-25 10:23:40 +0300

    Added ApiAuthentication

---
+++ FlowManager/FlowManager.Client/ApiAuthenticationStateProvider.cs
@@ -0,0 +1,68 @@
+using System.Security.Claims;
+using System.Net.Http.Json;
+using FlowManager.Domain.Entities;
+using Microsoft.AspNetCore.Components.Authorization;
+
+public class ApiAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private readonly HttpClient _httpClient;
+    private string _email; // Variabilă de instanță pentru a stoca email-ul
+
+    public ApiAuthenticationStateProvider(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public void SetEmail(string email)
+    {
+        _email = email;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"/api/Users/email/{_email}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            var user = await response.Content.ReadFromJsonAsync<User>();
+            Console.WriteLine(user.Email);
+
+            if (user == null || string.IsNullOrEmpty(user.UserName))
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            // Creează o identitate cu cereri (claims) folosind informațiile utilizatorului
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                // Adaugă cereri suplimentare după necesitate, de exemplu, roluri, email, etc.
+            }, "apiauth");
+
+            return new AuthenticationState(new ClaimsPrincipal(identity));
+        }
+        catch (Exception ex)
+        {
+            // Loghează detaliile excepției aici dacă ai un mecanism de logare
+            Console.WriteLine($"Error retrieving authentication state: {ex.Message}");
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+    }
+
+    public void NotifyUserAuthentication(string email)
+    {
+        SetEmail(email);
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+
+    public void NotifyUserLogout()
+    {
+        var anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        NotifyAuthenticationStateChanged(Task.FromResult(anonymous));
+    }
+}
+
