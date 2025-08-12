11e1b6e2e148091ed6cb996de9e63888d85df479
Arustei Andrei <arusteiandrew@gmail.com>
2025-08-12 13:38:02 +0300

    [update] endpoints for form template

---
+++ FlowManager/FlowManager.Application/Interfaces/IFormTemplateService.cs
@@ -0,0 +1,20 @@
+﻿using FlowManager.Application.DTOs.Requests.FormTemplate;
+using FlowManager.Application.DTOs.Responses;
+using FlowManager.Application.DTOs.Responses.FormTemplate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowManager.Application.Interfaces
+{
+    public interface IFormTemplateService
+    {
+        Task<PagedResponseDto<FormTemplateResponseDto>> GetAllFormTemplatesQueriedAsync(QueriedFormTemplateRequestDto payload);
+        Task<FormTemplateResponseDto?> GetFormTemplateByIdAsync(Guid id);
+        Task<FormTemplateResponseDto> PostFormTemplateAsync(PostFormTemplateRequestDto payload);
+        Task<FormTemplateResponseDto?> DeleteFormTemplateAsync(Guid id);
+        Task<FormTemplateResponseDto?> PatchFormTemplateAsync(PatchFormTemplateRequestDto payload);
+    }
+}
