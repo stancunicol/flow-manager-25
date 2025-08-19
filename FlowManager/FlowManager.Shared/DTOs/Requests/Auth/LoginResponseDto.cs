58783c80757f09c68c741671a6aec8c1370e148e
Stancu Nicol-Alexia <nicol.stancu@student.unitbv.ro>
2025-08-19 11:38:11 +0300

    For login page.

---
+++ FlowManager/FlowManager.Shared/DTOs/Requests/Auth/LoginResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowManager.Shared.DTOs.Requests.Auth
+{
+    public class LoginResponseDto
+    {
+        public string Message { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
